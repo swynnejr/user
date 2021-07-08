9010e9f755383a6b0ce497ec4f0c04a621033504
swynnejr <saucersam@gmail.com>
2021-07-08 00:36:24 -0500

    display_user_balance doesnt work

--- user.py
+++ user.py
@@ -46,6 +46,24 @@ class User:		# here's what we have so far
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
     	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+    def make_withdrawl(self, amount): # takes an argument that is the amount of the withdrawl
+        self.account_balance -= amount # the specific user's account decreases by the amount of the value withdrawn
+    def display_user_balance(self): #  <<< This doesn't work
+        print(self.name)
+        print(self.account_balance)
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
+
+travis = User("Travis Douglass", "TDugzzz@HaloReach.com")
+maddox = User("Maddox Womble", "MadFox420@WSMFP.com")
+
+guido.make_deposit(100)
+guido.make_deposit(200)
+monty.make_deposit(50)
+# print(guido.account_balance)	# output: 300
+# print(monty.account_balance)	# output: 50
+
+#checking display user balance method that doesnt work, but I dont get why it needs to be made when the above works fine...
+
+# monty.display_user_balance # <<<< Doesnt do what its supposed to
